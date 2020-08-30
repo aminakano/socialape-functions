cde67b3d3d514629e198f06e7b2ef9d4b4d7d469
Ami Nakano <ami.nakano@outlook.com>
2020-08-30 23:14:31 +0200

    signup logic WIP

---
+++ dbschema.js
@@ -0,0 +1,13 @@
+// Reference
+
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-08-30T20:05:53.985Z",
+      likeCount: 5,
+      commentCount: 2
+    },
+  ],
+};
--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,42 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
+const app = require("express")();
 
 admin.initializeApp();
 
-const express = require("express");
-const app = express();
+const config = {
+  apiKey: "AIzaSyCbiNIhP030IgJwqk97yElx9iT2Y_XgO7g",
+  authDomain: "socialape-75cc1.firebaseapp.com",
+  databaseURL: "https://socialape-75cc1.firebaseio.com",
+  projectId: "socialape-75cc1",
+  storageBucket: "socialape-75cc1.appspot.com",
+  messagingSenderId: "947673988292",
+  appId: "1:947673988292:web:cd372c80b4895c1acb5d91",
+  measurementId: "G-0M72K1SQDB",
+}
+
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
 
 app.get("/screams", (req, res) => { //1st param is the name of route & 2nd param is the handler
   admin
     .firestore()
     .collection("screams")
+    .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
       let screams = [];
       data.forEach((doc) => {
-        screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount,
+        });
       });
       return res.json(screams);
     })
@@ -22,13 +44,11 @@ app.get("/screams", (req, res) => { //1st param is the name of route & 2nd param
 })
 
 app.post("/scream",(req, res) => {
-  if(req.method !== "POST") {
-    return res.status(400).json({ error: "Method not allowed" });
-  }
+
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   }
 
   admin.firestore()
@@ -43,6 +63,23 @@ app.post("/scream",(req, res) => {
     })
 })
 
-// https://baseurl.com/api/ 
+// https://baseurl.com/api/
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
 
-exports.api = functions.https.onRequest(app);
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  .then(data => {
+    return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`})
+  })
+  .catch(err => {
+    console.err(err);
+    return res.status(500).json({ error: err.code })
+  })
+}) 
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
