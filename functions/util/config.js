a5b855c760e53aea139ebfa4381bd36892f91be6
Ami Nakano <ami.nakano@outlook.com>
2020-09-06 13:16:41 +0200

    setup routes

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCbiNIhP030IgJwqk97yElx9iT2Y_XgO7g",
+  authDomain: "socialape-75cc1.firebaseapp.com",
+  databaseURL: "https://socialape-75cc1.firebaseio.com",
+  projectId: "socialape-75cc1",
+  storageBucket: "socialape-75cc1.appspot.com",
+  messagingSenderId: "947673988292",
+  appId: "1:947673988292:web:cd372c80b4895c1acb5d91",
+  measurementId: "G-0M72K1SQDB",
+}
