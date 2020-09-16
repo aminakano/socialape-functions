67a9afde7e16d8e970adfba9936564eb748940be
Ami Nakano <ami.nakano@outlook.com>
2020-09-16 23:21:18 +0200

    frontend started - navbar created

---
+++ client/src/pages/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class login extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>login page</h1>
+      </div>
+    );
+  }
+}
+
+export default login;
