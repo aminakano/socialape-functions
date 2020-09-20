d63745d16ba263f484e79b1f7e78b1eec48d844b
Ami Nakano <ami.nakano@outlook.com>
2020-09-20 23:00:28 +0200

    login page validation WIP

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,22 +2,40 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/core";
 import PropTypes from "prop-types";
 import appIcon from "../images/icon.png"
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const styles = {
   form: {
     textAlign: "center",
   },
   image: {
-    margin: "20px auto"
+    margin: "20px auto",
   },
   pageTitle: {
-
+    margin: "10px auto",
   },
+  TextField: {
+    margin: "10px auto",
+  },
+  button: {
+    marginTop: 20,
+    position: "relative"
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: "0.5rem",
+  },
+  progress: {
+    position: "absolute"
+  }
 };
 
 
@@ -32,7 +50,28 @@ class login extends Component {
     };
   }
   handleSubmit = (event) => {
-    console.log("hi");
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    axios.post("/login", userData)
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          loading: false
+        });
+        this.props.history.push("/"); //homepage
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      })
   }
 
   handleChange = (event) => {
@@ -43,6 +82,8 @@ class login extends Component {
 
   render() {
     const { classes } = this.props;
+    const { errors, loading } = this.state;
+
     return (
       <div className="App">
         <Grid container className={classes.form}>
@@ -58,6 +99,8 @@ class login extends Component {
                 name="email"
                 type="email"
                 label="Email"
+                helperText={errors.email}
+                error={errors.email ? true : false}
                 className={classes.TextField}
                 value={this.state.email}
                 onChange={this.handleChange}
@@ -68,12 +111,31 @@ class login extends Component {
                 name="password"
                 type="password"
                 label="Password"
+                helperText={errors.password}
+                error={errors.password ? true : false}
                 className={classes.TextField}
                 value={this.state.password}
                 onChange={this.handleChange}
                 fullWidth
               />
-              <Button type="submit" variant="contained" color="primary" className={classes.button}/>
+              {errors.general && (
+                <Typography variant="body2" className={classes.customError}>
+                  {errors.general}
+                </Typography>
+              )}
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Log in
+                {loading && <CircularProgress className={classes.progress} size={30} />}
+              </Button>
+              <br />
+              <small>
+                Don't have an account? <Link to="/signup">Sign up</Link>
+              </small>
             </form>
           </Grid>
           <Grid item sm />
